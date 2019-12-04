08b4f04d02a95ff455d7998afe6002e2bbd1f1a6
pratiksha kathiriya <pratikshakakathiriya22597@gmail.com>
2019-12-04 18:50:43 -0500

    Initial Commit

---
+++ Pratiksha_MAPD714_Assignment2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pratiksha_MAPD714_Assignment2
+//
+//  Created by Pratiksha Kathiriya on 2019-12-04.
+//  Copyright © 2019 CentennialCollege. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
