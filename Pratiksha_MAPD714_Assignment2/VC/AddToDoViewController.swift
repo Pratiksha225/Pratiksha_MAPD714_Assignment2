c7652f86b6f0e264ffcdfaf3ba590f18eceab2a1
pratiksha kathiriya <pratikshakakathiriya22597@gmail.com>
2019-12-04 23:17:43 -0500

    Add To Do

---
+++ Pratiksha_MAPD714_Assignment2/VC/AddToDoViewController.swift
@@ -0,0 +1,34 @@
+//
+//  AddToDoViewController.swift
+//  Pratiksha_MAPD714_Assignment2
+//
+//  Created by Pratiksha Kathiriya on 2019-12-04.
+//  Copyright © 2019 CentennialCollege. All rights reserved.
+//
+
+import UIKit
+
+class AddToDoViewController: UIViewController
+{
+    
+    @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+    }
+    
+
+    @IBAction func cancelButton(_ sender: UIButton)
+    {
+        
+    }
+    
+    
+    @IBAction func doneButton(_ sender: UIButton)
+    {
+        
+    }
+}
---
+++ Pratiksha_MAPD714_Assignment2/VC/ToDoTableViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ToDoTableViewController.swift
+//  Pratiksha_MAPD714_Assignment2
+//
+//  Created by Pratiksha Kathiriya on 2019-12-04.
+//  Copyright © 2019 CentennialCollege. All rights reserved.
+//
+
+import UIKit
+
+class ToDoTableViewController: UITableViewController
+{
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int
+    {
+       
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+}
+
+    
